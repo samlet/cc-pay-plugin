eb019e1f5573ec9240ce84303921fd6871e27b3f
Samlet Wu <xiaofei.wu@gmail.com>
2021-04-10 14:50:50 +0800

    cc-pay init-version

---
+++ src/main/java/com/bluecc/pay/SeedResources.java
@@ -0,0 +1,42 @@
+package com.bluecc.pay;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.protobuf.util.JsonFormat;
+import io.grpc.examples.routeguide.Feature;
+import io.grpc.examples.routeguide.RouteGuideUtil;
+import org.apache.ofbiz.service.DispatchContext;
+import org.apache.ofbiz.service.GenericServiceException;
+import org.apache.ofbiz.service.ServiceUtil;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class SeedResources {
+    public static List<Map<String, Object>> getResources() throws IOException {
+        List<Feature> features= RouteGuideUtil.parseFeatures(RouteGuideUtil.getDefaultFeaturesFile());
+        ObjectMapper objectMapper=new ObjectMapper();
+
+        List<Map<String, Object>> result=new ArrayList<>();
+        for(Feature f:features) {
+            String json = JsonFormat.printer().print(f);
+            Map<String, Object> mapValues = objectMapper.readValue(json,
+                    new TypeReference<Map<String, Object>>() {});
+            result.add(mapValues);
+        }
+        return result;
+    }
+
+    public static Map<String, Object> retrieveResources(DispatchContext ctx, Map<String, ? extends Object> context) throws GenericServiceException {
+        Map<String, Object> result = ServiceUtil.returnSuccess();
+        try {
+            result.put("resources", getResources());
+        } catch (IOException e) {
+            throw new GenericServiceException(e.getMessage());
+        }
+        return result;
+    }
+}
+
---
+++ src/test/java/com/bluecc/pay/PayTests.java
@@ -0,0 +1,11 @@
+package com.bluecc.pay;
+
+import org.junit.Test;
+
+public class PayTests {
+    @Test
+    public void testUtil(){
+        System.out.println("==> default instance: "+
+                io.grpc.examples.routeguide.Rectangle.getDefaultInstance());
+    }
+}
