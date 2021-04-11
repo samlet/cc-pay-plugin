c4941d75aa7b06d57457a93f6a9e67baeb1bf16a
Samlet Wu <xiaofei.wu@gmail.com>
2021-04-11 17:52:54 +0800

    add config service impl

--- src/main/java/com/bluecc/pay/SrvRoutines.java
+++ src/main/java/com/bluecc/pay/SrvRoutines.java
@@ -1,5 +1,9 @@
 package com.bluecc.pay;
 
+import com.bluecc.packer.routines.ConfigItem;
+import com.bluecc.packer.routines.RoutinesGrpc;
+import com.bluecc.packer.routines.StringValue;
+import com.bluecc.packer.routines.ValueObject;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import io.grpc.examples.helloworld.GreeterGrpc;
@@ -7,6 +11,7 @@ import io.grpc.examples.helloworld.HelloReply;
 import io.grpc.examples.helloworld.HelloRequest;
 import io.grpc.stub.StreamObserver;
 import org.apache.ofbiz.base.container.ContainerException;
+import org.apache.ofbiz.base.util.UtilProperties;
 
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
@@ -15,8 +20,8 @@ import java.util.logging.Logger;
 /**
  * Server that manages startup/shutdown of a {@code Greeter} server.
  */
-public class SimpleSrv extends SrvBase {
-    private static final Logger logger = Logger.getLogger(SimpleSrv.class.getName());
+public class SrvRoutines extends SrvBase {
+    private static final Logger logger = Logger.getLogger(SrvRoutines.class.getName());
 
     private Server server;
 
@@ -26,6 +31,7 @@ public class SimpleSrv extends SrvBase {
         try {
             server = ServerBuilder.forPort(port)
                     .addService(new GreeterImpl())
+                    .addService(new RoutinesImpl())
                     .build()
                     .start();
         } catch (IOException e) {
@@ -40,7 +46,7 @@ public class SimpleSrv extends SrvBase {
                 // Use stderr here since the logger may have been reset by its JVM shutdown hook.
                 System.err.println("*** shutting down gRPC server since JVM is shutting down");
                 try {
-                    SimpleSrv.this.stop();
+                    SrvRoutines.this.stop();
                 } catch (ContainerException e) {
                     e.printStackTrace(System.err);
                 }
@@ -74,7 +80,7 @@ public class SimpleSrv extends SrvBase {
      * Main launches the server from the command line.
      */
     public static void main(String[] args) throws InterruptedException, ContainerException {
-        final SimpleSrv server = new SimpleSrv();
+        final SrvRoutines server = new SrvRoutines();
         server.start();
         server.blockUntilShutdown();
     }
@@ -88,4 +94,22 @@ public class SimpleSrv extends SrvBase {
             responseObserver.onCompleted();
         }
     }
+
+    static class RoutinesImpl extends RoutinesGrpc.RoutinesImplBase{
+
+        @Override
+        public void getConfig(ConfigItem request, StreamObserver<ValueObject> responseObserver) {
+            ValueObject.Builder vo=ValueObject.newBuilder();
+            // String protocol = UtilProperties.getPropertyValue("jsse", "java.protocol.handler.pkgs", "NONE");
+            vo.putData("timestamp", ValueObject.Value.newBuilder()
+                    .setIntValue(System.currentTimeMillis()).build());
+            String itemVal=UtilProperties.getPropertyValue(request.getItem(), request.getKey(), "NONE");
+            vo.putData(request.getItem()+"|"+request.getKey(), ValueObject.Value.newBuilder()
+                    .setStringValue(itemVal).build());
+
+            responseObserver.onNext(vo.build());
+            responseObserver.onCompleted();
+        }
+    }
 }
+
