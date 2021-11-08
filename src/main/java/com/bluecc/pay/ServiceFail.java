5533f866c74579f5f806dfa5f72aa8ffa6eb2bf6
Samlet Wu <xiaofei.wu@gmail.com>
2021-11-08 13:50:51 +0800

    add function registers

--- src/main/java/com/bluecc/pay/ServiceFail.java
+++ src/main/java/com/bluecc/pay/ServiceFail.java
@@ -3,6 +3,7 @@ package com.bluecc.pay;
 import java.util.Map;
 
 public class ServiceFail extends Exception {
+    private static final long serialVersionUID = 1L;
     public ServiceFail(String message, Map<String, Object> serviceResult) {
         super(message);
         this.serviceResult = serviceResult;
